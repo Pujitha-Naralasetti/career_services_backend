a9b0ff1f1ef99668d7868d432e897ebd360ed217
Pujitha <pujitha.naralasetti@eagles.oc.edu>
2024-07-01 10:15:45 -0500

    inital setup

---
+++ app/models/user.model.js
@@ -0,0 +1,32 @@
+const { saltSize, keySize } = require("../authentication/crypto");
+
+module.exports = (sequelize, Sequelize) => {
+  const User = sequelize.define("user", {
+    firstName: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    lastName: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    isActive: {
+      type: Sequelize.BOOLEAN,
+      defaultValue: true,
+    },
+    password: {
+      type: Sequelize.BLOB,
+      allowNull: false,
+    },
+    salt: {
+      type: Sequelize.BLOB,
+      allowNull: false,
+    },
+  });
+
+  return User;
+};
