77b7d57d15a54b3124e7fa6945e26e8a26b9f28b
Pujitha <pujitha.naralasetti@eagles.oc.edu>
2024-07-08 01:33:32 -0500

    Adding student view profile

--- app/routes/user.routes.js
+++ app/routes/user.routes.js
@@ -4,5 +4,8 @@ module.exports = (app) => {
 // Create a new User
 router.post("/users/", User.create);
  
+ // Retrieve a single User with id
+ router.get("/users/:id", [authenticateRoute], User.findOne);
+ 
   app.use("/resumeapi", router);
 };
