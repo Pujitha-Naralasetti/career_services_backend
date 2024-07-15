b6bbf639d2886b81463584f369fb4725b6d83598
JagadeeswariAdapa <jagadeeswari.adapa@eagles.oc.edu>
2024-07-15 13:22:23 -0500

    Award_CRUD_changes

--- app/models/award.model.js
+++ app/models/award.model.js
@@ -1,18 +1,18 @@
 module.exports = (sequelize, Sequelize) => {
-    const Award = sequelize.define("awards", {
-      title: {
-        type: Sequelize.STRING,
-        allowNull: false,
-      },
-      awardingBody: {
-        type: Sequelize.STRING,
-      },
-      date: {
-        type: Sequelize.DATE,
-      },
-      description: {
-        type: Sequelize.TEXT,
-      },
-    });
-    return Award
-  };
+  const Award = sequelize.define("awards", {
+    title: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    issuedBy: {
+      type: Sequelize.STRING,
+    },
+    date: {
+      type: Sequelize.DATE,
+    },
+    description: {
+      type: Sequelize.TEXT,
+    },
+  });
+  return Award
+};
