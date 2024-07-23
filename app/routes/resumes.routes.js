877fc6e5188d7ac80cc3fc0a96b5358237db04c0
JagadeeswariAdapa <jagadeeswari.adapa@eagles.oc.edu>
2024-07-23 09:27:26 -0500

    resume template changes

--- app/routes/resumes.routes.js
+++ app/routes/resumes.routes.js
@@ -1,23 +1,25 @@
 module.exports = (app) => {
-    const Resumes = require("../controllers/resumes.controller.js");
-    const { authenticateRoute } = require("../authentication/authentication");
-    var router = require("express").Router();
+  const Resumes = require("../controllers/resumes.controller.js");
+  const { authenticateRoute } = require("../authentication/authentication");
+  var router = require("express").Router();
+
+  // Create a new Resumes Details for Student
+  router.post("/resumes/:userId", [authenticateRoute], Resumes.create);
+
+  //Get resume templates
+  router.get("/resumes/getAllSampleTemplates", [authenticateRoute], Resumes.findTemplates);
+
+  // Get Resumes details of student
+  router.get("/resumes/user/:userId", [authenticateRoute], Resumes.findByUserId);
+
+  // Get one resumes record by Id
+  router.get("/resumes/:id", [authenticateRoute], Resumes.findById);
+
+  // update resumes record by Id
+  router.put("/resumes/:id", [authenticateRoute], Resumes.update);
+
+// delete resumes record by Id
+  router.delete("/resumes/:id", [authenticateRoute], Resumes.delete);
   
-    // Create a new Resumes Details for Student
-    router.post("/resumes/:userId", [authenticateRoute], Resumes.create);
-  
-    // Get Resumes details of student
-    router.get("/resumes/user/:userId", [authenticateRoute], Resumes.findByUserId);
-  
-    // Get one resumes record by Id
-    router.get("/resumes/:id", [authenticateRoute], Resumes.findById);
-  
-    // update resumes record by Id
-    router.put("/resumes/:id", [authenticateRoute], Resumes.update);
-  
-  // delete resumes record by Id
-    router.delete("/resumes/:id", [authenticateRoute], Resumes.delete);
-    
-    app.use("/resumeapi", router);
-  };
-  
+  app.use("/resumeapi", router);
+};
---
+++ app/utils/templates.js
@@ -0,0 +1,62 @@
+const templateData = [
+  {
+    profileSummary:
+      "Highly motivated and results-oriented software engineer with 5+ years of experience in designing, developing, and implementing complex web applications. Skilled in a variety of programming languages and frameworks, including JavaScript, React, and Node.js. Proven ability to work independently and as part of a team to deliver high-quality solutions on time and within budget.",
+    userId: 1,
+    personalInfo:
+      '{"name":"First Name Last Name","address":"123 Main Street, Anytown, CA 12345","phone":"(555) 555-5555","email":"john.doe@example.com","linkedIn":"https://www.linkedin.com/in/johndoe"}',
+    education:
+      '[{"institutionName":"Oklahoma Christian University","degree":"Bachelor of Arts /Bachelor of Science","course":"XX /B.B.A","startDate":"2024-07-22T03:02:22.564Z","endDate":"2024-07-22T03:02:22.564Z","gpa":"3.8","address":"Oklahoma City, OK","awards":["Pat on the back"],"courseWork":[]}]',
+    experience:
+      '[{"company":"Employer","designation":"Your Title","isInternship":false,"jobType":"Full Time","startDate":"2024-07-22T03:02:22.564Z","endDate":"2024-07-22T03:02:22.564Z","address":"City, State","experiencePoints":["Accomplished {X} as measured by {Y}, by doing {Z}","Action + Project/Problem + Results = Accomplishment","Three bullet points is sufficient for the most relevant position."]},{"company":"Employer","designation":"Your Title","isInternship":false,"jobType":"Full Time","startDate":"2024-07-22T03:02:22.564Z","endDate":"2024-07-22T03:02:22.564Z","address":"City, State","experiencePoints":["Filed papers and organized 100+ employee documents a month to ensure compliance with employment law & successfully passed the annual audit all three years in the role.","Led a 5-person leadership team to increase student participation by 100% from 50 to 100 members by creating a stronger social media presence."]},{"company":"Employer","designation":"Your Title","isInternship":false,"jobType":"Full Time","startDate":"2024-07-22T03:02:22.564Z","endDate":"2024-07-22T03:02:22.564Z","address":"City, State","experiencePoints":["Developed concepts and designs for 10 clients, including consumer products, electronics, and enterprise technology, utilizing Adobe Photoshop and Illustrator.","Collaborated with the IT team to develop an online application submission and tracking system, reducing cost by 10%."]}]',
+    skills:
+      '{"hardSkills":["JavaScript (ES6+)","React","Node.js","Express.js","MongoDB","Git"],"languageSkills":["English (fluent)","Spanish (conversational)"]}',
+    templateType: 1,
+    userId: null,
+  },
+  {
+    profileSummary:
+      "Tells the reader the purpose of your resume in 3 (2-4 is okay) sentences • Should identify the kind of work you want to perform • If seeking an internship or co-op, include the time period when you are available to start. Example: Seeking a summer 2020 internship in Marketing or Public Relations.",
+    userId: 1,
+    personalInfo:
+      '{"name":"Gopi Mekala","address":"4-79, Venkatapur\\nDuggondi, Warangal rural, 506331","phone":"09133886211","email":"gm@gmail.com","linkedIn":"https://www.linkedin.com/in/johndoe"}',
+    education:
+      '[{"institutionName":"Oklahoma Christian University","degree":"Bachelor of Arts /Bachelor of Science","course":"XX /B.B.A","startDate":"2024-07-22T14:37:53.485Z","endDate":"2024-07-22T14:37:53.485Z","gpa":"3.8","address":"Oklahoma City, OK"}]',
+    experience:
+      '[{"company":"Name of Company","designation":"Job Role","isInternship":false,"jobType":"Full Time","startDate":"2024-07-22T14:37:53.485Z","endDate":"2024-07-22T14:37:53.485Z","address":"City, State","experiencePoints":["Accomplished {X} as measured by {Y}, by doing {Z}","Action + Project/Problem + Results = Accomplishment","Three bullet points is sufficient for the most relevant position."]},{"company":"Name of Company","designation":"Job Role","isInternship":false,"jobType":"Full Time","startDate":"2024-07-22T14:37:53.485Z","endDate":"2024-07-22T14:37:53.485Z","address":"City, State","experiencePoints":["Filed papers and organized 100+ employee documents a month to ensure compliance with employment law & successfully passed the annual audit all three years in the role.","Led a 5-person leadership team to increase student participation by 100% from 50 to 100 members by creating a stronger social media presence."]},{"company":"Name of Company","designation":"Job Role","isInternship":false,"jobType":"Full Time","startDate":"2024-07-22T14:37:53.485Z","endDate":"2024-07-22T14:37:53.485Z","address":"City, State","experiencePoints":["Developed concepts and designs for 10 clients, including consumer products, electronics, and enterprise technology, utilizing Adobe Photoshop and Illustrator.","Collaborated with the IT team to develop an online application submission and tracking system, reducing cost by 10%."]}]',
+    templateType: 2,
+    userId: null,
+  },
+  {
+    profileSummary:
+      "Highly motivated and results-oriented software engineer with 5+ years of experience in designing, developing, and implementing complex web applications. Skilled in a variety of programming languages and frameworks, including JavaScript, React, and Node.js. Proven ability to work independently and as part of a team to deliver high-quality solutions on time and within budget.",
+    userId: 1,
+    personalInfo:
+      '{"name":"First Name Last Name","address":"123 Main Street, Anytown, CA 12345","phone":"(555) 555-5555","email":"john.doe@example.com","linkedIn":"https://www.linkedin.com/in/johndoe"}',
+    education:
+      '[{"institutionName":"Oklahoma Christian University","degree":"Bachelor of Arts /Bachelor of Science","course":"XX /B.B.A","startDate":"2024-07-22T03:02:22.564Z","endDate":"2024-07-22T03:02:22.564Z","gpa":"3.8","address":"Oklahoma City, OK","awards":["Pat on the back"],"courseWork":[]}]',
+    experience:
+      '[{"company":"Employer","designation":"Your Title","isInternship":false,"jobType":"Full Time","startDate":"2024-07-22T03:02:22.564Z","endDate":"2024-07-22T03:02:22.564Z","address":"City, State","experiencePoints":["Accomplished {X} as measured by {Y}, by doing {Z}","Action + Project/Problem + Results = Accomplishment","Three bullet points is sufficient for the most relevant position."]},{"company":"Employer","designation":"Your Title","isInternship":false,"jobType":"Full Time","startDate":"2024-07-22T03:02:22.564Z","endDate":"2024-07-22T03:02:22.564Z","address":"City, State","experiencePoints":["Filed papers and organized 100+ employee documents a month to ensure compliance with employment law & successfully passed the annual audit all three years in the role.","Led a 5-person leadership team to increase student participation by 100% from 50 to 100 members by creating a stronger social media presence."]},{"company":"Employer","designation":"Your Title","isInternship":false,"jobType":"Full Time","startDate":"2024-07-22T03:02:22.564Z","endDate":"2024-07-22T03:02:22.564Z","address":"City, State","experiencePoints":["Developed concepts and designs for 10 clients, including consumer products, electronics, and enterprise technology, utilizing Adobe Photoshop and Illustrator.","Collaborated with the IT team to develop an online application submission and tracking system, reducing cost by 10%."]}]',
+    skills:
+      '{"hardSkills":["JavaScript (ES6+)","React","Node.js","Express.js","MongoDB","Git"],"languageSkills":["English (fluent)","Spanish (conversational)"]}',
+    templateType: 3,
+    userId: null,
+  },
+  {
+    profileSummary:
+      "Highly motivated and results-oriented software engineer with 5+ years of experience in designing, developing, and implementing complex web applications. Skilled in a variety of programming languages and frameworks, including JavaScript, React, and Node.js. Proven ability to work independently and as part of a team to deliver high-quality solutions on time and within budget.",
+    userId: 1,
+    personalInfo:
+      '{"name":"First Name Last Name","address":"123 Main Street, Anytown, CA 12345","phone":"(555) 555-5555","email":"john.doe@example.com","linkedIn":"https://www.linkedin.com/in/johndoe"}',
+    education:
+      '[{"institutionName":"Oklahoma Christian University","degree":"Bachelor of Arts /Bachelor of Science","course":"XX /B.B.A","startDate":"2024-07-22T03:02:22.564Z","endDate":"2024-07-22T03:02:22.564Z","gpa":"3.8","address":"Oklahoma City, OK","awards":["Pat on the back"],"courseWork":[]}]',
+    experience:
+      '[{"company":"Employer","designation":"Your Title","isInternship":false,"jobType":"Full Time","startDate":"2024-07-22T03:02:22.564Z","endDate":"2024-07-22T03:02:22.564Z","address":"City, State","experiencePoints":["Accomplished {X} as measured by {Y}, by doing {Z}","Action + Project/Problem + Results = Accomplishment","Three bullet points is sufficient for the most relevant position."]},{"company":"Employer","designation":"Your Title","isInternship":false,"jobType":"Full Time","startDate":"2024-07-22T03:02:22.564Z","endDate":"2024-07-22T03:02:22.564Z","address":"City, State","experiencePoints":["Filed papers and organized 100+ employee documents a month to ensure compliance with employment law & successfully passed the annual audit all three years in the role.","Led a 5-person leadership team to increase student participation by 100% from 50 to 100 members by creating a stronger social media presence."]},{"company":"Employer","designation":"Your Title","isInternship":false,"jobType":"Full Time","startDate":"2024-07-22T03:02:22.564Z","endDate":"2024-07-22T03:02:22.564Z","address":"City, State","experiencePoints":["Developed concepts and designs for 10 clients, including consumer products, electronics, and enterprise technology, utilizing Adobe Photoshop and Illustrator.","Collaborated with the IT team to develop an online application submission and tracking system, reducing cost by 10%."]}]',
+    skills:
+      '{"hardSkills":["JavaScript (ES6+)","React","Node.js","Express.js","MongoDB","Git"],"languageSkills":["English (fluent)","Spanish (conversational)"]}',
+    templateType: 4,
+    userId: null,
+  },
+];
+
+module.exports = templateData;
